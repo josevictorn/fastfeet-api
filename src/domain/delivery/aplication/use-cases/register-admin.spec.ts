76e1aca2e72e85832c1aeacd10a512b509b5fdb9
José Victor <josevictornascimento2016@gmail.com>
2025-02-20 22:21:28 -0300

    test: :white_check_mark: created the register admin use case test

--- src/domain/delivery/aplication/use-cases/register-admin.spec.ts
+++ src/domain/delivery/aplication/use-cases/register-admin.spec.ts
@@ -1,3 +1,61 @@
-test('1 plus 1', () => {
-  expect(1 + 1).toBe(2)
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { InMemoryAdminsRepository } from 'test/repositories/in-memory-admins-repository'
+import { RegisterAdminUseCase } from './register-admin'
+import { AdminAlreadyExistsError } from './erros/admin-already-exists-error'
+
+let inMemoryAdminsRepository: InMemoryAdminsRepository
+let fakeHasher: FakeHasher
+
+let sut: RegisterAdminUseCase
+
+describe('Register Admin', () => {
+  beforeEach(() => {
+    inMemoryAdminsRepository = new InMemoryAdminsRepository()
+    fakeHasher = new FakeHasher()
+
+    sut = new RegisterAdminUseCase(inMemoryAdminsRepository, fakeHasher)
+  })
+
+  it('should be able to register a new admin', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      cpf: '12345678911',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      admin: inMemoryAdminsRepository.items[0],
+    })
+  })
+
+  it('should hash admin password upon registration', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      cpf: '12345678911',
+      password: '123456',
+    })
+
+    const hashedPassword = await fakeHasher.hash('123456')
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryAdminsRepository.items[0].password).toEqual(hashedPassword)
+  })
+
+  it('should not be able to register admin with same cpf', async () => {
+    await sut.execute({
+      name: 'John Doe',
+      cpf: '12345678911',
+      password: '123456',
+    })
+
+    const result = await sut.execute({
+      name: 'John Doe',
+      cpf: '12345678911',
+      password: '123456',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(AdminAlreadyExistsError)
+  })
 })
---
+++ test/repositories/in-memory-admins-repository.ts
@@ -0,0 +1,20 @@
+import { AdminsRepository } from '@/domain/delivery/aplication/repositories/admins-repository'
+import { Admin } from '@/domain/delivery/enterprise/entities/admin'
+
+export class InMemoryAdminsRepository implements AdminsRepository {
+  public items: Admin[] = []
+
+  async findByCpf(cpf: string) {
+    const admin = this.items.find((item) => item.cpf === cpf)
+
+    if (!admin) {
+      return null
+    }
+
+    return admin
+  }
+
+  async create(admin: Admin) {
+    this.items.push(admin)
+  }
+}
